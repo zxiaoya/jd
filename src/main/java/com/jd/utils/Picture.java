519491602912bafac36184d01a61b94f4848b7c5
joke <zxiaoya0906@163.com>
2018-05-11 18:05:01 +0800

    增加图片展示图形化

--- src/main/java/com/jd/utils/Picture.java
+++ src/main/java/com/jd/utils/Picture.java
@@ -2,6 +2,9 @@ package com.jd.utils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Picture extends JFrame {
 
@@ -30,10 +33,7 @@ public class Picture extends JFrame {
         this.getContentPane().add(button);
         this.setVisible(true);
 
-        while (showTime-- < 0)
-        {
-                System.exit(0);
-        }
+
     }
 
     public static void shutdown () {
@@ -44,5 +44,90 @@ public class Picture extends JFrame {
         new Picture("D://QR.png");
         Thread.sleep(2000);
         shutdown();
+    }
+
+
+    public static Long dateDiff(String startTime, String endTime,
+                                String format, String str) {
+        // 按照传入的格式生成一个simpledateformate对象
+        SimpleDateFormat sd = new SimpleDateFormat(format);
+        long nd = 1000 * 24 * 60 * 60;// 一天的毫秒数
+        long nh = 1000 * 60 * 60;// 一小时的毫秒数
+        long nm = 1000 * 60;// 一分钟的毫秒数
+        long ns = 1000;// 一秒钟的毫秒数
+        long diff;
+        long day = 0;
+        long hour = 0;
+        long min = 0;
+        long sec = 0;
+        // 获得两个时间的毫秒时间差异
+        try {
+            diff = sd.parse(endTime).getTime() - sd.parse(startTime).getTime();
+            day = diff / nd;// 计算差多少天
+            hour = diff % nd / nh + day * 24;// 计算差多少小时
+            min = diff % nd % nh / nm + day * 24 * 60;// 计算差多少分钟
+            sec = diff % nd % nh % nm / ns;// 计算差多少秒
+            // 输出结果
+            System.out.println("时间相差：" + day + "天" + (hour - day * 24) + "小时"
+                    + (min - day * 24 * 60) + "分钟" + sec + "秒。");
+            System.out.println("hour=" + hour + ",min=" + min);
+            if (str.equalsIgnoreCase("h")) {
+                return hour;
+            } else if (str.equalsIgnoreCase("m")){
+                return min;
+            } else {
+                return sec;
+            }
+
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (str.equalsIgnoreCase("h")) {
+            return hour;
+        } else {
+            return min;
+        }
+    }
+    public final static String NORM_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    public static Long dateDiff(String endTime,
+                                String format, String str) {
+        // 按照传入的格式生成一个simpledateformate对象
+        SimpleDateFormat sd = new SimpleDateFormat(format);
+        long nd = 1000 * 24 * 60 * 60;// 一天的毫秒数
+        long nh = 1000 * 60 * 60;// 一小时的毫秒数
+        long nm = 1000 * 60;// 一分钟的毫秒数
+        long ns = 1000;// 一秒钟的毫秒数
+        long diff;
+        long day = 0;
+        long hour = 0;
+        long min = 0;
+        long sec = 0;
+        // 获得两个时间的毫秒时间差异
+        try {
+            diff = sd.parse(endTime).getTime() - sd.parse(new SimpleDateFormat(NORM_DATETIME_PATTERN).format(new Date())).getTime();
+            day = diff / nd;// 计算差多少天
+            hour = diff % nd / nh + day * 24;// 计算差多少小时
+            min = diff % nd % nh / nm + day * 24 * 60;// 计算差多少分钟
+            sec = diff % nd % nh % nm / ns;// 计算差多少秒
+            // 输出结果
+            System.out.println("时间相差：" + day + "天" + (hour - day * 24) + "小时"
+                    + (min - day * 24 * 60) + "分钟" + sec + "秒。");
+            System.out.println("hour=" + hour + ",min=" + min);
+            if (str.equalsIgnoreCase("h")) {
+                return hour;
+            } else {
+                return min;
+            }
+
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (str.equalsIgnoreCase("h")) {
+            return hour;
+        } else {
+            return min;
+        }
     }
 }
